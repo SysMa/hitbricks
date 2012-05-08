69619c0dee2971550cfe8f4cb6491d94272d862a
shiqingma@gmail.com <shiqingma@gmail.com@cffbff93-a8eb-f12d-1460-fad86f1c6637>
2012-05-08 13:51:43 +0000

    重构(⊙o⊙)…删除了Controller这个东西，因为我画棒子没有小毛的好，虽然我感觉现在这个东西也画的不是很好。现在我们的元素都有了，就差运动的逻辑了。后期还需要解决的问题：1。运动的逻辑；2.界面元素的放置；3.速度速度啊，Kinect太烂了吧？

--- HitBrick_WinForm/HitBrick_WinForm/KinectFormImpl.cs
+++ HitBrick_WinForm/HitBrick_WinForm/KinectFormImpl.cs
@@ -11,7 +11,6 @@ namespace HitBrick_WinForm
     enum ERRORTYPE { CONSOLE_PRINT, MESSAGE_BOX };
     public partial class KinectForm : Form
     {
-
         Render render;
 
         private void KinectForm_Disposed(object sender, EventArgs e)
@@ -21,38 +20,38 @@ namespace HitBrick_WinForm
 
         private void KinectForm_Load(object sender, EventArgs e)
         {
-            //KinectSensor sensor;
-            //render = new Render();
+            KinectSensor sensor;
+            render = new Render();
             ////存在kinect体感设备，取第一个,否则退出
-            //if (KinectSensor.KinectSensors.Count != 0)
-            //{
-            //    sensor = KinectSensor.KinectSensors[0];
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Kinect is not ready!");
-            //    Error("Window_Loaded", "Kinect is not ready", ERRORTYPE.CONSOLE_PRINT);
-            //    return;
-            //}
+            if (KinectSensor.KinectSensors.Count != 0)
+            {
+                sensor = KinectSensor.KinectSensors[0];
+            }
+            else
+            {
+                MessageBox.Show("Kinect is not ready!");
+                Error("Window_Loaded", "Kinect is not ready", ERRORTYPE.CONSOLE_PRINT);
+                return;
+            }
 
-            //if (sensor != null)
-            //{
-            //    render.SetSensor(sensor);
-            //    //初始化设备流
-            //    render.InitStream();
-            //    //开启体感设备
-            //    render.StartSensor();
-            //}
+            if (sensor != null)
+            {
+                render.SetSensor(sensor);
+                //初始化设备流
+                render.InitStream();
+                //开启体感设备
+                render.StartSensor();
+            }
             timer_Tick(sender, e);
             initPosition();
 
-            //render.BindComponent(ref manImage, ref manPanel);
+            render.BindComponent(ref manImage, splitContainer1.Panel1);
 
-            //this.SetStyle(ControlStyles.UserPaint, true);
-            //this.SetStyle(ControlStyles.DoubleBuffer, true);
-            //this.SetStyle(ControlStyles.AllPaintingInWmPaint, true); 
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.DoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
 
-            //render.RunRender();
+            render.RunRender();
         }
 
         private void initPosition()
@@ -86,328 +85,5 @@ namespace HitBrick_WinForm
                     break;
             }
         }
-  
-    }
-
-    /// <summary>
-    /// 检测人物骨骼点的检测和呈现人物抠图
-    /// </summary>
-    class Render
-    {
-        KinectSensor sensor;
-        DepthImageStream depthImageStream;
-        private short[] depthPixelData;
-        private byte[] colorPixelData;
-        int depthImageStride;
-        double manImageWidth = 0;
-        double manImageHeight = 0;
-
-        Bitmap depthImageBitmap;
-        Rectangle depthImageBitmapRect;
-
-        private Point rightHand;
-        private Point leftHand;
-        private Point head;
-
-
-        Graphics manImageGraphic;
-        Rectangle barRect;
-        SolidBrush barBrush;
-        //注册图形
-        PictureBox manImage;
-        PictureBox imgBar;
-        Panel manPanel;
-        public void BindComponent(ref PictureBox manShape, ref Panel manPanel)
-        {
-            this.manImage = manShape;
-            this.manPanel = manPanel;
-            //this.imgBar = imgBar;
-
-            this.manImage.Width = (int)(3 / 4.0 * this.manImage.Height);
-            manImageHeight = this.manImage.Height;
-            manImageWidth = this.manImage.Width;
-            manImageGraphic = manShape.CreateGraphics();
-            barBrush = new SolidBrush(Color.Red);
-            manImageGraphic.FillRectangle(barBrush, barRect);
-        }
-
-        public void SetSensor(KinectSensor sensor)
-        {
-            this.sensor = sensor;
-        }
-
-        public void InitStream()
-        {
-            //开启colorstream
-            ColorImageStream colorStream = sensor.ColorStream;
-            if (colorStream == null)
-            {
-                MessageBox.Show("Kinect is not ready!!");
-                Error("InitStream", "Kinect is not ready", ERRORTYPE.CONSOLE_PRINT);
-                return;
-            }
-            //开启colorstream
-            sensor.ColorStream.Enable(ColorImageFormat.RgbResolution1280x960Fps12);
-
-            //开启skeletonstream
-            //设置参数
-            var parameters = new TransformSmoothParameters
-            {
-                Smoothing = 0.75f,
-                Correction = 0.0f,
-                Prediction = 0.0f,
-                JitterRadius = 0.05f,
-                MaxDeviationRadius = 0.04f
-            };
-            sensor.SkeletonStream.Enable(parameters);
-
-            //开启depthstream
-            depthImageStream = sensor.DepthStream;
-            sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-
-            depthImageBitmap = new Bitmap(depthImageStream.FrameWidth, depthImageStream.FrameHeight);
-            depthImageBitmapRect = new Rectangle(0, 0, (int)depthImageStream.FrameWidth, (int)depthImageStream.FrameHeight);
-            depthImageStride = depthImageStream.FrameWidth * 4;
-            depthPixelData = new short[sensor.DepthStream.FramePixelDataLength];
-            colorPixelData = new byte[sensor.ColorStream.FramePixelDataLength];
-        }
-
-        public void StartSensor()
-        {
-            sensor.Start();
-        }
-
-        public void CloseSensor()
-        {
-            sensor.Stop();
-            sensor.Dispose();
-        }
-
-        public void RunRender()
-        {
-            //添加骨骼事件处理
-            sensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(sensor_SkeletonFrameReady);
-            //添加深度事件处理
-            sensor.DepthFrameReady += new EventHandler<DepthImageFrameReadyEventArgs>(sensor_DepthFrameReady);
-        }
-
-        private void sensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
-        {
-            manImageGraphic.FillRectangle(barBrush, barRect);
-            using (DepthImageFrame dframe = e.OpenDepthImageFrame())
-            {
-                using (ColorImageFrame cframe = sensor.ColorStream.OpenNextFrame(2))
-                {
-                    if (dframe != null && cframe != null)
-                    {
-                        RenderScreen(dframe, cframe);
-                    }
-                }
-            }
-        }
-
-        private void sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
-        {
-            manImageGraphic.FillRectangle(barBrush, barRect);
-            SkeletonFrame skeletonFrame = e.OpenSkeletonFrame();
-
-            if (skeletonFrame == null)
-                return;
-            Skeleton[] skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
-            skeletonFrame.CopySkeletonDataTo(skeletonData);
-
-            //get the first tracked skeleton
-            Skeleton skeleton = (from s in skeletonData
-
-                                 where s.TrackingState == SkeletonTrackingState.Tracked
-
-                                 select s).FirstOrDefault();
-            if (skeleton == null)
-                return;
-
-            rightHand = getDisplayPosition(skeleton.Joints[JointType.HandRight]);
-            leftHand = getDisplayPosition(skeleton.Joints[JointType.HandLeft]);
-            head = getDisplayPosition(skeleton.Joints[JointType.Head]);
-            //SetBarPosition(skeleton.Joints[JointType.HandLeft], skeleton.Joints[JointType.HandRight], imgBar);
-        }
-
-
-        private void RenderScreen(DepthImageFrame depthFrame, ColorImageFrame colorFrame)
-        {
-            if (sensor != null && colorFrame != null && depthFrame != null)
-            {
-                int count = 0;          //所有捕获的点的个数
-                double avgPosition = 0;  //深度平均中心（决定人物方框的位置）
-                int depthPixelIndex;
-                int playerIndex;
-                int colorPixelIndex;
-                int playerImageIndex = 0;
-                int colorStride = colorFrame.BytesPerPixel * colorFrame.Width;//5120
-                int bytesPerPixel = 4;
-                byte[] playerImage = new byte[sensor.DepthStream.FrameHeight * depthImageStride];//1 228 800
-
-                ColorImagePoint colorPoint;
-
-                depthFrame.CopyPixelDataTo(depthPixelData);//307200
-                colorFrame.CopyPixelDataTo(colorPixelData);//4 915 200
-
-                for (int depthY = 0; depthY < depthFrame.Height; depthY++)
-                {
-                    for (int depthX = 0; depthX < depthFrame.Width; depthX++, playerImageIndex += bytesPerPixel)
-                    {
-                        depthPixelIndex = depthX + (depthY * depthFrame.Width);
-                        playerIndex = depthPixelData[depthPixelIndex] & DepthImageFrame.PlayerIndexBitmask;
-                        if (playerIndex != 0)
-                        {
-                            colorPoint = sensor.MapDepthToColorImagePoint(depthFrame.Format, depthX, depthY, depthPixelData[depthPixelIndex], colorFrame.Format);
-                            colorPixelIndex = (colorPoint.X * colorFrame.BytesPerPixel) + (colorPoint.Y * colorStride);
-
-                            if (colorPixelIndex < sensor.ColorStream.FramePixelDataLength)
-                            {
-                                avgPosition = avgPosition * count + depthX;
-                                count++;
-                                avgPosition /= count;
-
-                                playerImage[playerImageIndex] = colorPixelData[colorPixelIndex];             //Blue    
-                                playerImage[playerImageIndex + 1] = colorPixelData[colorPixelIndex + 1];     //Green
-                                playerImage[playerImageIndex + 2] = colorPixelData[colorPixelIndex + 2];     //Red
-                                playerImage[playerImageIndex + 3] = 0xFF;    //Alpha
-                            }
-                        }
-                    }
-                }
-                BitmapData bmapdata = depthImageBitmap.LockBits(depthImageBitmapRect, ImageLockMode.WriteOnly, depthImageBitmap.PixelFormat);
-
-                IntPtr ptr = bmapdata.Scan0;
-                System.Runtime.InteropServices.Marshal.Copy(playerImage, 0, ptr, playerImage.Length);
-
-                depthImageBitmap.UnlockBits(bmapdata);
-                manImage.SizeMode = PictureBoxSizeMode.StretchImage;
-                manImage.Image = depthImageBitmap;//depthImageBitmap;              
-
-                //map to panel
-                avgPosition = (avgPosition) * (manImageWidth / depthFrame.Width);// - manImage.Height / depthFrame.Height);
-                avgPosition = avgPosition * ((double)manPanel.Width / manImage.Width) - manImage.Width / 2.0;
-                manImage.Location = new Point((int)(avgPosition + manPanel.Location.X), manPanel.Height - manImage.Height);
-            }
-        }
-
-        private void SetBarPosition(Joint leftHand, Joint rightHand, PictureBox imgBar = null, Panel panel = null)
-        {
-            Point leftP = getDisplayPosition(leftHand);
-            Point rightP = getDisplayPosition(rightHand);
-
-            int w = (int)Math.Sqrt((rightP.Y - leftP.Y) * (rightP.Y - leftP.Y) + (rightP.X - leftP.X) * (rightP.X - leftP.X));
-
-            if (rightP.X != leftP.X)
-            {
-                float angle = (float)Math.Atan(((double)(rightP.Y - leftP.Y)) / (rightP.X - leftP.X));
-                angle = (float)(angle * 180 / Math.PI);
-                barRect = new Rectangle(new Point(0, 0), new Size(w, 10));
-                manImageGraphic.ResetTransform();
-                manImageGraphic.TranslateTransform(leftP.X, leftP.Y);
-                manImageGraphic.RotateTransform(angle, MatrixOrder.Prepend);
-
-                manImageGraphic.FillRectangle(barBrush, barRect);
-            }
-        }
-
-        private Point getDisplayPosition(Joint joint)
-        {
-            ColorImagePoint colorPoint = sensor.MapSkeletonPointToColor(joint.Position,
-                                        ColorImageFormat.RgbResolution640x480Fps30);
-            // map back to skeleton.Width & skeleton.Height
-            return new Point((int)((manImageWidth) * colorPoint.X / 640.0),
-                (int)((manImageHeight) * colorPoint.Y / 480.0));
-        }
-
-        private void Error(String OperationName, String ErrorInfo, ERRORTYPE ErrorType = ERRORTYPE.CONSOLE_PRINT)
-        {
-            switch (ErrorType)
-            {
-                case ERRORTYPE.CONSOLE_PRINT:
-                    Console.WriteLine("Error:{1} existes in operation:{0}", OperationName, ErrorInfo);
-                    break;
-                case ERRORTYPE.MESSAGE_BOX:
-                    MessageBox.Show("Error:" + ErrorInfo + " exists in operation " + OperationName);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        public double GetBarWidth()
-        {
-            return barRect.Width;
-        }
-
-        public Point GetBarLocation()
-        {
-            return barRect.Location;
-        }
-
-        public Rectangle GetBarRect()
-        {
-            return barRect;
-        }
-
-        public Point RightHand
-        {
-            get { return rightHand; }
-            set { rightHand = value; }
-        }
-        public Point LeftHand
-        {
-            get { return leftHand; }
-            set { leftHand = value; }
-        }
-        public Point Head
-        {
-            get { return head; }
-            set { head = value; }
-        }
-    };
- 
- 
-    /// <summary>
-    /// A Class that contains some static utility functions. 
-    /// </summary>
-    class Utilities
-    {
-        /// <summary>
-        /// Creates a new Image containing the same image only rotated
-        /// </summary>
-        /// <param name=""image"">The <see cref=""System.Drawing.Image"/"> to rotate
-        /// <param name=""offset"">The position to rotate from.
-        /// <param name=""angle"">The amount to rotate the image, clockwise, in degrees
-        /// <returns>A new <see cref=""System.Drawing.Bitmap"/"> of the same size rotated.</see>
-        /// <exception cref=""System.ArgumentNullException"">Thrown if <see cref=""image"/"> 
-        /// is null.</see>
-        public static Bitmap RotateImage(Image image, PointF offset, float angle)
-        {
-            if (image == null)
-                throw new ArgumentNullException("image");
-
-            //create a new empty bitmap to hold rotated image
-            Bitmap rotatedBmp = new Bitmap(image.Width, image.Height);
-            rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            //make a graphics object from the empty bitmap
-            Graphics g = Graphics.FromImage(rotatedBmp);
-
-            //Put the rotation point in the center of the image
-            g.TranslateTransform(offset.X, offset.Y);
-
-            //rotate the image
-            g.RotateTransform(angle);
-
-            //move the image back
-            g.TranslateTransform(-offset.X, -offset.Y);
-
-            //draw passed in image onto graphics object
-            g.DrawImage(image, new PointF(0, 0));
-
-            return rotatedBmp;
-        }
-    };
+    } 
 }
