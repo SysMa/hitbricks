783c0b6575d9e92505daf9c7a58f934199cb4289
shiqingma@gmail.com <shiqingma@gmail.com@cffbff93-a8eb-f12d-1460-fad86f1c6637>
2012-05-22 04:52:54 +0000

    对关卡中图的位置略作调整。

--- trunk/HitBrick_WinForm/HitBrick_WinForm/Stages.cs
+++ trunk/HitBrick_WinForm/HitBrick_WinForm/Stages.cs
@@ -9,13 +9,19 @@ namespace HitBrick_WinForm
         public void draw_stage_one()
         {
             int temp = 0;
+            int half_blank = (this.splitContainer1.Panel1.Width - _width) / 2;
             Random rd = new Random();
-            for (int i = 100; i < _height; i += 18)
+            for (int i = 100; i < _height - 18 * 2; i += 18)
             {
                 temp += 18;
-                for (int j = temp - 18; j < _width - temp; j += 40)
+                int edge = 0;
+                if (i >= 5)
                 {
-                    Rectangle Rect = new Rectangle(j, i, 40, 18);
+                    edge = 18;
+                }
+                for (int j = temp - 18; j < _width - temp - edge; j += 40)
+                {
+                    Rectangle Rect = new Rectangle(half_blank + j, i, 40, 18);
                     Brick_Type temp_brick = new Brick_Type();
                     temp_brick.rectangle = Rect;
                     temp_brick.type = rd.Next() % 3;
@@ -26,6 +32,7 @@ namespace HitBrick_WinForm
         }
         public void draw_stage_two()
         {
+            int half_blank = (this.splitContainer1.Panel1.Width - _width) / 2;
             Random rd = new Random();
             for (int i = 66; i < _height - 18 * 3; i += 18)   //_width: 400 、  _height: 300 、 每个砖块的高度18
             {
@@ -35,7 +42,7 @@ namespace HitBrick_WinForm
                     int y = (i - 66) / 18;
                     if (x + y == 9 || x == y || y == 5 || y == 0 || y == 9 || x == 0 || x == 9)
                     {
-                        Rectangle Rect = new Rectangle(j, i, 40, 18);
+                        Rectangle Rect = new Rectangle(half_blank + j, i, 40, 18);
                         Brick_Type temp_brick = new Brick_Type();
                         temp_brick.rectangle = Rect;
                         temp_brick.type = rd.Next() % 3;
