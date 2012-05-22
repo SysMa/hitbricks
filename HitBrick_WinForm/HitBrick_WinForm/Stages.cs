7712414132b67378832d2934e6803f80eeed54cf
shiqingma@gmail.com <shiqingma@gmail.com@cffbff93-a8eb-f12d-1460-fad86f1c6637>
2012-05-22 14:44:11 +0000

    新的关卡设计。

--- HitBrick_WinForm/HitBrick_WinForm/Stages.cs
+++ HitBrick_WinForm/HitBrick_WinForm/Stages.cs
@@ -8,20 +8,23 @@ namespace HitBrick_WinForm
     {
         public void draw_stage_one()
         {
-            int temp = 0;
             int half_blank = (this.splitContainer1.Panel1.Width - _width) / 2;
             Random rd = new Random();
-            for (int i = begin; i < _height - 18 * 2; i += 18)
+            int bricksNumberPerLine = 10;
+            for (int i = 66, j = 0; i < _height - 18 * 3; i += 18, j += 20, bricksNumberPerLine--)
             {
-                temp += 18;
-                for (int j = temp - 18; j < _width - temp; j += 40)
+                int bricksNumberCurrentLine = bricksNumberPerLine;
+                for (int x = j; x < _width; x += 40)
                 {
-                    Rectangle Rect = new Rectangle(half_blank + j, i, 40, 18);
-                    Brick_Type temp_brick = new Brick_Type();
-                    temp_brick.rectangle = Rect;
-                    temp_brick.type = rd.Next() % 3;
-                    temp_brick.pictureBox = new PictureBox();
-                    Rects.Add(temp_brick);
+                    if (bricksNumberCurrentLine-- > 0)
+                    {
+                        Rectangle Rect = new Rectangle(half_blank + x, i, 40, 18);
+                        Brick_Type temp_brick = new Brick_Type();
+                        temp_brick.rectangle = Rect;
+                        temp_brick.type = rd.Next() % 3;
+                        temp_brick.pictureBox = new PictureBox();
+                        Rects.Add(temp_brick);
+                    }
                 }
             }
         }
@@ -30,13 +33,37 @@ namespace HitBrick_WinForm
         {
             int half_blank = (this.splitContainer1.Panel1.Width - _width) / 2;
             Random rd = new Random();
-            for (int i = begin; i < _height - 18 * 3; i += 18)   //_width: 400 、  _height: 300 、 每个砖块的高度18
+            for (int i = 66; i < _height - 18 * 3; i += 18)   //_width: 400 、  _height: 300 、 每个砖块的高度18
             {
                 for (int j = 0; j < _width; j += 40)
                 {
                     int x = j / 40;
                     int y = (i - 66) / 18;
                     if (x + y == 9 || x == y || y == 5 || y == 0 || y == 9 || x == 0 || x == 9)
+                    {
+                        Rectangle Rect = new Rectangle(half_blank + j, i, 40, 18);
+                        Brick_Type temp_brick = new Brick_Type();
+                        temp_brick.rectangle = Rect;
+                        temp_brick.type = rd.Next() % 3;
+                        temp_brick.pictureBox = new PictureBox();
+                        Rects.Add(temp_brick);
+                    }
+                }
+            }
+        }
+
+        public void draw_stage_three()
+        {
+            int half_blank = (this.splitContainer1.Panel1.Width - _width) / 2;
+            Random rd = new Random();
+            for (int i = 66; i < _height - 18 * 3; i += 18)   //_width: 400 、  _height: 300 、 每个砖块的高度18
+            {
+                for (int j = 0; j < _width; j += 40)
+                {
+                    int x = j / 40;
+                    int y = (i - 66) / 18;
+                    if (x == 0 || x == 9 || y == 0 || y == 9 || x + y == 4 || x - y == 5 ||
+                                   (1 < x && x < 8 && y == 5) || (x == 2 && y > 4) || (x == 7 && y > 4))
                     {
                         Rectangle Rect = new Rectangle(half_blank + j, i, 40, 18);
                         Brick_Type temp_brick = new Brick_Type();
