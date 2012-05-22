b7fa6854e99b88528da6671acc42ae834908046d
shiqingma@gmail.com <shiqingma@gmail.com@cffbff93-a8eb-f12d-1460-fad86f1c6637>
2012-05-22 01:59:22 +0000

    添加了第二关，求小毛更新

--- HitBrick_WinForm/HitBrick_WinForm/Stages.cs
+++ HitBrick_WinForm/HitBrick_WinForm/Stages.cs
@@ -24,5 +24,26 @@ namespace HitBrick_WinForm
                 }
             }
         }
+        public void draw_stage_two()
+        {
+            Random rd = new Random();
+            for (int i = 66; i < _height - 18 * 3; i += 18)   //_width: 400 、  _height: 300 、 每个砖块的高度18
+            {
+                for (int j = 0; j < _width; j += 40)
+                {
+                    int x = j / 40;
+                    int y = (i - 66) / 18;
+                    if (x + y == 9 || x == y || y == 5 || y == 0 || y == 9 || x == 0 || x == 9)
+                    {
+                        Rectangle Rect = new Rectangle(j, i, 40, 18);
+                        Brick_Type temp_brick = new Brick_Type();
+                        temp_brick.rectangle = Rect;
+                        temp_brick.type = rd.Next() % 3;
+                        temp_brick.pictureBox = new PictureBox();
+                        Rects.Add(temp_brick);
+                    }
+                }
+            }
+        }
     }
 }
